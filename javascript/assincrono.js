68849f48880d80d14d2c8583afcd57bed960dada
Vanessa Santana <vanessa_santanatoledo@outlook.com>
2022-05-18 21:04:03 -0300

    Local storage

--- javascript/assincrono.js
+++ javascript/assincrono.js
@@ -1,63 +1,86 @@
-// console.log("teste");
-// console.log("teste 1");
-// fetch('https://jsonplaceholder.typicode.com/posts')
-// .then(response => response.json())
-// .then(json => console.log(json));
-// console.log("teste 2");
-// console.log("teste 3");
+// // console.log("teste");
+// // console.log("teste 1");
+// // fetch('https://jsonplaceholder.typicode.com/posts')
+// // .then(response => response.json())
+// // .then(json => console.log(json));
+// // console.log("teste 2");
+// // console.log("teste 3");
 
-// const json = '{"teste":"vanessa"}';
-// const teste = JSON.parse(json);
-// console.log(JSON.stringify(teste));
+// // const json = '{"teste":"vanessa"}';
+// // const teste = JSON.parse(json);
+// // console.log(JSON.stringify(teste));
 
-// Utilizando a API https://viacep.com.br/ws/${CEP}/json/
-// crie um formulário onde o usuário pode digitar o cep
-// e o endereço completo é retornado ao clicar em buscar
+// // Utilizando a API https://viacep.com.br/ws/${CEP}/json/
+// // crie um formulário onde o usuário pode digitar o cep
+// // e o endereço completo é retornado ao clicar em buscar
 
-let button = document.querySelector("button");
-let input = document.querySelector("input");
-let cidade = document.querySelector(".cidade");
-let bairro = document.querySelector(".bairro");
-let estado = document.querySelector(".estado");
+// let button = document.querySelector("button");
+// let input = document.querySelector("input");
+// let cidade = document.querySelector(".cidade");
+// let bairro = document.querySelector(".bairro");
+// let estado = document.querySelector(".estado");
 
-button.addEventListener("click", async function (event) {
-  event.preventDefault();
-  const endereco = await fetch(`https://viacep.com.br/ws/${input.value}/json/`);
-  const dados = await endereco.json();
-  cidade.textContent = dados.localidade;
-  estado.textContent = dados.uf;
-  bairro.textContent = dados.bairro;  
+// button.addEventListener("click", async function (event) {
+//   event.preventDefault();
+//   const endereco = await fetch(`https://viacep.com.br/ws/${input.value}/json/`);
+//   const dados = await endereco.json();
+//   cidade.textContent = dados.localidade;
+//   estado.textContent = dados.uf;
+//   bairro.textContent = dados.bairro;  
+// });
+
+// // Utilizando a API https://blockchain.info/ticker
+// // retorne no DOM o valor de compra da bitcoin em reais.
+// // atualize este valor a cada 30s
+// const bitcoin = document.querySelector(".bitcoin");
+// async function valorBitcoin() {
+//   const valorBitcoin = await fetch("https://blockchain.info/ticker");
+//   const dados = await valorBitcoin.json();
+//   bitcoin.innerText = dados.BRL.buy.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+// }
+// valorBitcoin();
+// setInterval( async function () {
+//   valorBitcoin();
+// }, 30000);
+
+// // Utilizando a API https://api.chucknorris.io/jokes/random
+// // retorne uma piada randomica do chucknorris, toda vez que
+// // clicar em próxima
+// const jokes = document.querySelector(".piada");
+// const next = document.querySelector(".next");
+
+// async function piadas () {
+//   const piada = await fetch("https://api.chucknorris.io/jokes/random");
+//   const json = await piada.json();
+//   jokes.innerText = json.value;
+// }
+
+// piadas();
+
+// next.addEventListener("click", function () {
+//  piadas();
+// });
+
+const btn = document.querySelectorAll("button");
+const produtosExistenteSacola = JSON.parse(localStorage.getItem("Sacola"));
+const sacola = document.querySelector(".sacola");
+let produtos = [];    
+btn.forEach(element => {
+  element.addEventListener("click", function() {
+    let produto = this.getAttribute("data-id");
+    console.log(produtosExistenteSacola);
+    if (produtosExistenteSacola.lenght) {
+      produtosExistenteSacola.forEach(function(item) {
+
+        console.log(item);
+        if(item.id != produto) {
+          produtos.push({"id": produto});
+        }
+      });
+    } else {
+      produtos.push({"id": produto});
+    }
+    localStorage.setItem("Sacola", JSON.stringify(produtos));
+    // sacola.innerText = qtdSacola;
+  })
 });
-
-// Utilizando a API https://blockchain.info/ticker
-// retorne no DOM o valor de compra da bitcoin em reais.
-// atualize este valor a cada 30s
-const bitcoin = document.querySelector(".bitcoin");
-async function valorBitcoin() {
-  const valorBitcoin = await fetch("https://blockchain.info/ticker");
-  const dados = await valorBitcoin.json();
-  bitcoin.innerText = dados.BRL.buy.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
-}
-valorBitcoin();
-setInterval( async function () {
-  valorBitcoin();
-}, 30000);
-
-// Utilizando a API https://api.chucknorris.io/jokes/random
-// retorne uma piada randomica do chucknorris, toda vez que
-// clicar em próxima
-const jokes = document.querySelector(".piada");
-const next = document.querySelector(".next");
-
-async function piadas () {
-  const piada = await fetch("https://api.chucknorris.io/jokes/random");
-  const json = await piada.json();
-  jokes.innerText = json.value;
-}
-
-piadas();
-
-next.addEventListener("click", function () {
- piadas();
-});
-  
